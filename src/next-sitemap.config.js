5da01f20c5ea38b529c02c0a7aa03dad4bef9c19
alpacheen <a.muir.1987@hotmail.com>
2025-07-31 15:18:28 +1000

    add sitemap config

--- src/next-sitemap.config.js
+++ src/next-sitemap.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://snippetly.vercel.app',
-    generateRobotsTxt: true,
-    exclude: ['/api/*', '/admin/*', '/_next/*'],
-    robotsTxtOptions: {
-      policies: [
-        {
-          userAgent: '*',
-          allow: '/',
-          disallow: ['/api/', '/admin/', '/_next/'],
-        },
-      ],
-      additionalSitemaps: [
-        `${process.env.NEXT_PUBLIC_BASE_URL || 'https://snippetly.vercel.app'}/sitemap.xml`,
-      ],
-    },
-  }
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://snippetly.vercel.app",
+  generateRobotsTxt: true,
+  exclude: ["/api/*", "/admin/*", "/_next/*"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/admin/", "/_next/"],
+      },
+    ],
+    additionalSitemaps: [
+      `${
+        process.env.NEXT_PUBLIC_BASE_URL || "https://snippetly.vercel.app"
+      }/sitemap.xml`,
+    ],
+  },
+};
